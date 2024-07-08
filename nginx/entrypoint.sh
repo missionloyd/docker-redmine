d5a0771a42986b86aa65272db2dea27a1eca3e9a
Luke Macy <lukemacy1@gmail.com>
2024-07-08 15:07:26 -0600

    added certbot email env variable, updated nginx init

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# First attempt to renew certificates
-certbot renew --nginx
+# Ensure certificates exist
+if [ ! -f /etc/letsencrypt/live/redmine.powertheoryinc.com/fullchain.pem ]; then
+    certbot certonly --standalone -d redmine.powertheoryinc.com --non-interactive --agree-tos -m ${CERTBOT_EMAIL}
+fi
 
 # Start nginx in the foreground
 nginx -g 'daemon off;'
 
-# schedule a cron job or a loop to periodically renew certificates
+# Schedule a cron job or a loop to periodically renew certificates
 while :; do
     sleep 12h
     certbot renew --nginx
--- run_docker.sh
+++ run_docker.sh
@@ -3,5 +3,8 @@
 echo "killing old docker processes"
 docker compose rm -fs
 
+echo "setting development env variables in './.env.local'"
+export DOCKER_COMPOSE_ENV_FILE=./.env.local
+
 echo "building docker containers"
-docker compose up --build -d
+docker compose --env-file $DOCKER_COMPOSE_ENV_FILE up --build -d
