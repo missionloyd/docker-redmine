91ca55dcf26a35fd557fad9e8970940dc1d45733
Luke Macy <lukemacy1@gmail.com>
2024-07-08 17:05:37 -0600

    added automatic cert generation for ssl redmine

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -9,7 +9,18 @@ fi
 nginx -g 'daemon off;'
 
 # Schedule a cron job or a loop to periodically renew certificates
-while :; do
+while :; do    
+    openssl genrsa -out redmine.key 2048
+    openssl req -new -key redmine.key -out redmine.csr -subj "/C=US/ST=WY/L=Laramie/O=PowerTheoryInc/OU=Operations/CN=powertheoryinc.com"
+    openssl x509 -req -days 365 -in redmine.csr -signkey redmine.key -out redmine.crt
+    openssl dhparam -out dhparam.pem 2048
+
+    mkdir -p /etc/ssl/certs/
+    cp redmine.key /etc/ssl/certs/
+    cp redmine.crt /etc/ssl/certs/
+    cp dhparam.pem /etc/ssl/certs/
+    chmod 400 /etc/ssl/certs/redmine.key
+
     sleep 12h
     certbot renew --nginx
 done
