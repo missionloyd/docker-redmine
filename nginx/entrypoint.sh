d8bca4654bd24b0badc3e34c4a1d8f4ff3a026ac
Luke Macy <lukemacy1@gmail.com>
2024-07-08 20:28:04 -0600

    Revert "added certbot email env variable, updated nginx init"
    This reverts commit d5a0771a42986b86aa65272db2dea27a1eca3e9a.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,24 +3,12 @@
 # Ensure certificates exist
 if [ ! -f /etc/letsencrypt/live/redmine.powertheoryinc.com/fullchain.pem ]; then
     certbot certonly --standalone -d redmine.powertheoryinc.com --non-interactive --agree-tos -m ${CERTBOT_EMAIL}
-    # Generate key, CSR, and self-signed certificate
-    openssl genrsa -out /redmine.key 2048
-    openssl req -new -key /redmine.key -out /redmine.csr -subj "/C=US/ST=WY/L=Laramie/O=PowerTheoryInc/OU=Operations/CN=powertheoryinc.com"
-    openssl x509 -req -days 365 -in /redmine.csr -signkey /redmine.key -out /redmine.crt
-    openssl dhparam -out /dhparam.pem 2048
-
-    # Copy the generated files to the appropriate locations
-    mkdir -p /etc/ssl/certs/
-    cp /redmine.key /etc/ssl/certs/
-    cp /redmine.crt /etc/ssl/certs/
-    cp /dhparam.pem /etc/ssl/certs/
-    chmod 400 /etc/ssl/certs/redmine.key
 fi
 
 # Start nginx in the foreground
 nginx -g 'daemon off;'
 
-# Schedule a cron job or a loop to periodically renew certificates
+# schedule a cron job or a loop to periodically renew certificates
 while :; do
     sleep 12h
     certbot renew --nginx
